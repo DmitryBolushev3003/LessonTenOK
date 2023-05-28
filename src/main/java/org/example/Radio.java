475ae2110bf73ea5f4cf24bb5b39dee00bf0301c
Dima Bolushev <bdima3003@mail.ru>
2023-05-28 10:36:11 +0300

    LessonEleven

--- src/main/java/org/example/Radio.java
+++ src/main/java/org/example/Radio.java
@@ -1,20 +1,35 @@
 package org.example;
 
+
 public class Radio {
+    private int maxStation;
+    private int minStation = 0;
     private int currentStation;
+    private int minVolume = 0;
+    private int maxVolume = 100;
     private int currentVolume;
+
+    public Radio() {
+        maxStation = 9;
+    }
+
+    public Radio(int numberOfStation) {
+        maxStation = numberOfStation - 1;
+    }
 
     public int getCurrentStation() {
         return currentStation;
     }
 
     public void setCurrentStation(int currentStation) {
-        if (currentStation<0){
+        if (currentStation < minStation) {
             return;
-        };
-        if (currentStation>9){
+        }
+        ;
+        if (currentStation > maxStation) {
             return;
-        };
+        }
+        ;
         this.currentStation = currentStation;
     }
 
@@ -23,45 +38,44 @@ public class Radio {
     }
 
     public void setCurrentVolume(int currentVolume) {
-        if (currentVolume<0){
+        if (currentVolume < minVolume) {
             return;
         }
-        if (currentVolume>100){
+        if (currentVolume > maxVolume) {
             return;
         }
         this.currentVolume = currentVolume;
     }
 
-    public void nextStation(){
-        if (currentStation <9){
+    public void nextStation() {
+        if (currentStation < maxStation) {
             currentStation++;
-        }
-        else {
-            currentStation =0;
+        } else {
+            currentStation = minStation;
         }
     }
 
-    public void prevStation (){
-        if (currentStation>0){
+    public void prevStation() {
+        if (currentStation > minStation) {
             currentStation--;
-        }
-        else {currentStation = 9;
+        } else {
+            currentStation = maxStation;
         }
     }
-    public void nextVolume (){
-        if (currentVolume<100){
+
+    public void nextVolume() {
+        if (currentVolume < maxVolume) {
             currentVolume++;
-        }
-        else {
-            currentVolume = 100;
+        } else {
+            currentVolume = maxVolume;
         }
     }
-    public void prevVolume(){
-        if (currentVolume>0){
+
+    public void prevVolume() {
+        if (currentVolume > minVolume) {
             currentVolume--;
-        }
-        else {
-            currentVolume = 0;
+        } else {
+            currentVolume = minVolume;
         }
     }
 }
